ac99dbcbb17b2c8c0c0e2fb01b16e69d63e43405
Riwaj Shrestha <riwaj.shrestha111@gmail.com>
2024-05-01 11:13:45 +0545

    updating navbar

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
 
 urlpatterns = [
-    path('register/',views.user_list, name='list'),
+    path('register/',views.user_list, name='register'),
 ]
